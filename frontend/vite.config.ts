6faeb4109f22b9d3e7862079f24cd2fda3bf032b
Sarah Kwon <55962587+sarahkwon@users.noreply.github.com>
2024-10-06 18:23:59 -0700

    sk-3: dockerize frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+   },
+   server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    // origin: "http://0.0.0.0:3000", for some reason, this causes images to not load because of [[Failed to load resource: net::ERR_ADDRESS_INVALID]]
+   },
 })
