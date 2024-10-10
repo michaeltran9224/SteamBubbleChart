68cb7310c032dff08132332e3e46dc56d7cc8fe9
Michael Tran <michael.tran9224@gmail.com>
2024-10-10 16:21:04 -0700

    added getSteamUserGames api request and configured dockerfile

--- flask/README.md
+++ flask/README.md
@@ -1,2 +1,27 @@
-remember to use the venv interpreter
-venv\Scripts\Activate.ps1 to access the venv
+# Flask Backend Setup
+
+## Create .env file with the following variables:
+
+STEAM_API_KEY={insert your Steam API key here}
+FLASK_APP=hello.py
+FLASK_ENV=development (if you are developing) or FLASK_ENV=production (if your product is finished)
+
+### You can receive your own Steam API key via https://steamcommunity.com/dev/apikey
+
+## Docker prerequisites:
+
+Docker must be installed on your machine via https://www.docker.com/products/docker-desktop/
+
+## To build Docker image:
+
+docker-compose build --no-cache
+
+## To run the Docker container:
+
+docker-compose up -d (the -d is optional and makes the service run in the background so you can concurrently use your terminal)
+
+## To shut down the Docker container:
+
+docker-compose down
+
+### You will be able to access the flask api via http://127.0.0.1:5000/
--- flask/app/routes.py
+++ flask/app/routes.py
@@ -11,7 +11,7 @@ STEAM_API_KEY = os.getenv('STEAM_API_KEY')
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'michael'}
+    user = {'username': 'sk'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -25,6 +25,45 @@ def index():
     print(f"Using Steam API Key: {STEAM_API_KEY}")  # Debug output
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/steam/user/games/<steamId>', methods=['GET'])
+def getSteamUserGames(steamId):
+    url = f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={STEAM_API_KEY}&steamid={steamId}&include_appinfo=true&include_played_free_games=true&include_free_sub=true&include_extended_appinfo=false&format=json"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        games = data.get('response', {}).get('games', [])
+
+        if games:
+            game_list = []
+            for game in games:
+                app_id = game.get('appid')
+                name = game.get('name')
+                playtime_forever = game.get('playtime_forever', 0)
+                img_icon_url = game.get('img_icon_url')
+                image_url = f"http://media.steampowered.com/steamcommunity/public/images/apps/{app_id}/{img_icon_url}.jpg"
+
+                game_info = {
+                    'app_id': app_id,
+                    'name': name,
+                    'playtime_forever': playtime_forever,
+                    'img_icon_url': image_url
+                }
+
+                game_list.append(game_info)
+
+            return jsonify({'games': game_list})
+        else:
+            return jsonify({'error': 'No games found for this Steam ID'}), 404
+    else:
+        print(f"Error response from Steam API: {response.status_code}, {response.text}")
+        return jsonify({'error': 'Failed to fetch data from Steam API'}), 500
+        
+# REACT: fetch(`/steam/user/games/${steamId}`)
+    #games is a list btw
+
+#getplayersummary steam api call
 @app.route('/steam/user/<steamId>', methods=['GET'])
 def getSteamUser(steamId):
     url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_API_KEY}&steamids={steamId}&format=json"
@@ -46,7 +85,13 @@ def getSteamUser(steamId):
             realName = player.get('realname')
             lastLogoff = player.get('lastlogoff')
 
-            # create dictionary
+            #will return none if not set by user
+            locCountryCode = player.get('loccountrycode')
+            locStateCode = player.get('locstatecode')
+            locCityId = player.get('loccityid')
+
+
+            # create dictionary to assign information 
             player_info = {
                 'steam_id': steamId,
                 'persona_name': personaName,
@@ -55,6 +100,18 @@ def getSteamUser(steamId):
                 'real_name': realName,
                 'last_logoff': lastLogoff
             }
+
+            #conditional dictionary addition
+
+            if locCountryCode:
+                player_info['country_code'] = locCountryCode
+            if locStateCode:
+                player_info['state_code'] = locStateCode
+            if locCityId:
+                player_info['city_id'] = locCityId
+
+
+
             return jsonify(player_info)  # return as JSON
         else:
             return jsonify({'error': 'No players found for provided Steam IDs'}), 404 
